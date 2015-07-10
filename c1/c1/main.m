c90ef32cda48292b32b151ebc8273b80a1dc6059
junp <junpmail@163.com>
2015-07-10 23:14:17 +0800

    c1

---
+++ c1/c1/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  c1
+//
+//  Created by junp on 15-7-10.
+//  Copyright (c) 2015年 junp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+    
+        NSLog(@"Learn Object-C");
+        
+    }
+    
+    return 0;
+}
